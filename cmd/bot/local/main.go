b4bed30794a7ed27ee90acd86b9228459adf993e
Eduard Truuvaart <eduard.truuvaart.extern@prosiebensat1digital.de>
2023-01-31 11:08:44 +0000

    more changes

--- cmd/bot/local/main.go
+++ cmd/bot/local/main.go
@@ -6,11 +6,10 @@ import (
 	"os"
 
 	"github.com/EduardTruuvaart/web-observer/repository"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 var contentRepository *repository.DynamoContentRepository
@@ -28,24 +27,15 @@ func init() {
 }
 
 func main() {
-	lambda.Start(handleRequest)
-}
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
 
-func handleRequest(c context.Context, request events.APIGatewayProxyRequest) (string, error) {
-	// httpClient := &http.Client{}
-	// contentFetcher := service.NewContentFetcher(contentRepository, httpClient)
-
-	// bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// chatID := int64(000000000)
-	// msg := tgbotapi.NewMessage(chatID, "Hello World")
-	// _, err = bot.Send(msg)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	return "Hello World", nil
+	chatID := int64(000000000)
+	msg := tgbotapi.NewMessage(chatID, "Hello World")
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Panic(err)
+	}
 }
