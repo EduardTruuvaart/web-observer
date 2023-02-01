7759869e92576e462abcf30b47bbab420f28b043
Eduard Truuvaart <eduard.truuvaart.extern@prosiebensat1digital.de>
2023-02-01 19:11:34 +0000

    processor is added

---
+++ repository/bot_flow_repository.go
@@ -0,0 +1,76 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/EduardTruuvaart/web-observer/domain"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type BotFlowRepository interface {
+	FindByChatID(ctx context.Context, chatID int64) (domain.BotFlowState, error)
+	Save(ctx context.Context, chatID int64, state domain.BotFlowState) error
+}
+
+type DynamoBotFlowRepository struct {
+	db              dynamodb.Client
+	dynamoTableName string
+}
+
+func NewDynamoBotFlowRepository(db dynamodb.Client, dynamoTableName string) *DynamoBotFlowRepository {
+	return &DynamoBotFlowRepository{
+		db:              db,
+		dynamoTableName: dynamoTableName,
+	}
+}
+
+func (r *DynamoBotFlowRepository) FindByChatID(ctx context.Context, chatID int64) (domain.BotFlowState, error) {
+	params := &dynamodb.GetItemInput{
+		TableName: aws.String(r.dynamoTableName),
+		Key: map[string]types.AttributeValue{
+			"ChatID": &types.AttributeValueMemberN{Value: strconv.FormatInt(chatID, 10)},
+		},
+		ConsistentRead: aws.Bool(false),
+	}
+
+	result, err := r.db.GetItem(ctx, params)
+
+	if err != nil {
+		fmt.Printf("Got error calling dynamodb GetItem: %s\n", err)
+		return "", err
+	}
+
+	if len(result.Item) == 0 {
+		return domain.NotStarted, nil
+	}
+
+	state := result.Item["State"].(*types.AttributeValueMemberS).Value
+	return domain.BotFlowState(state), nil
+}
+
+func (r *DynamoBotFlowRepository) Save(ctx context.Context, chatID int64, state domain.BotFlowState) error {
+	now := time.Now().UTC()
+	formattedDate := now.Format(time.RFC3339)
+
+	input := dynamodb.PutItemInput{
+		TableName: aws.String(r.dynamoTableName),
+		Item: map[string]types.AttributeValue{
+			"ChatID":      &types.AttributeValueMemberN{Value: strconv.FormatInt(chatID, 10)},
+			"UpdatedDate": &types.AttributeValueMemberS{Value: formattedDate},
+			"State":       &types.AttributeValueMemberS{Value: string(state)},
+		},
+	}
+	_, err := r.db.PutItem(ctx, &input)
+
+	if err != nil {
+		fmt.Printf("Got error calling dynamodb PutItem: %s\n", err)
+		return err
+	}
+
+	return nil
+}
