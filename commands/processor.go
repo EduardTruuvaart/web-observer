2de9461502c81cb518a7bf5a1180ac887a9e4df0
Eduard Truuvaart <eduard.truuvaart.extern@prosiebensat1digital.de>
2023-02-01 13:14:25 +0000

    processor is added

---
+++ commands/processor.go
@@ -0,0 +1,106 @@
+package commands
+
+import (
+	"context"
+	"io"
+	"strings"
+
+	"github.com/EduardTruuvaart/web-observer/repository"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/pkg/errors"
+)
+
+// CommandProcessor processes a Telegram update message
+type CommandProcessor interface {
+	io.Closer
+	Process(ctx context.Context, update tgbotapi.Update) error
+}
+
+type TgCommandProcessor struct {
+	bot               *tgbotapi.BotAPI
+	contentRepository repository.ContentRepository
+}
+
+func NewProcessor(contentRepository repository.ContentRepository, bot *tgbotapi.BotAPI) (*TgCommandProcessor, error) {
+	return &TgCommandProcessor{
+		bot:               bot,
+		contentRepository: contentRepository,
+	}, nil
+}
+
+func (t *TgCommandProcessor) Close() error {
+	t.bot.StopReceivingUpdates()
+	return nil
+}
+
+// Process an event, any update happened in telegram bot will be processed by one entry point
+func (t TgCommandProcessor) Process(ctx context.Context, update tgbotapi.Update) error {
+	if update.Message != nil {
+
+		if update.Message.IsCommand() {
+			// this is a command, i.e. starting with /, for example, "/start" or "/track'
+			return t.processCommands(ctx, update.Message)
+		}
+
+		// just a plain message was sent
+		SendMsg(t.bot, update.Message.Chat.ID,
+			"Sorry, I don't understand you. Please use /start to get started.")
+		return nil
+	}
+
+	if update.CallbackQuery != nil {
+
+		// this is button click callback
+		//return t.processButtonCallback(ctx, update.CallbackQuery)
+	}
+
+	return nil
+}
+
+// SendMsg simply send a message to bot in Markdown format, general function
+func SendMsg(bot *tgbotapi.BotAPI, chatID int64, textMarkdown string) (tgbotapi.Message, error) {
+
+	if bot == nil {
+		return tgbotapi.Message{}, errors.New("Bot was not set")
+	}
+
+	msg := tgbotapi.NewMessage(chatID, textMarkdown)
+	msg.ParseMode = "Markdown"
+	msg.DisableWebPagePreview = true
+
+	// send the message
+	resp, err := bot.Send(msg)
+	if err != nil {
+		return resp, errors.Wrap(err, "Error sending message")
+	}
+
+	return resp, nil
+}
+
+// From DOCs: https://core.telegram.org/bots/api#markdownv2-style
+// esc
+// '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'
+// must be escaped with the preceding character '\'.`'
+func markdownEscape(s string) string {
+	replacer := strings.NewReplacer(
+		"_", "\\_",
+		"*", "\\*",
+		"[", "\\[",
+		"]", "\\]",
+		"(", "\\(",
+		")", "\\)",
+		"~", "\\~",
+		"`", "\\`",
+		">", "\\>",
+		"#", "\\#",
+		"+", "\\+",
+		"-", "\\-",
+		"=", "\\=",
+		"|", "\\|",
+		"{", "\\{",
+		"}", "\\}",
+		".", "\\.",
+		"!", "\\!",
+	)
+	return replacer.Replace(s)
+}
