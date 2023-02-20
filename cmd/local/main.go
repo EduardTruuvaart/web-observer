7dca339e579fe3960ea8f49b6d6399048ca16a52
Eduard Truuvaart <eduard.truuvaart.extern@prosiebensat1digital.de>
2023-02-20 17:03:57 +0000

    observer changes

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 
+	"github.com/EduardTruuvaart/web-observer/commands"
 	"github.com/EduardTruuvaart/web-observer/domain"
 	"github.com/EduardTruuvaart/web-observer/repository"
 	"github.com/EduardTruuvaart/web-observer/service"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
@@ -24,6 +27,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	db := *dynamodb.NewFromConfig(cfg)
 	s3Client := *s3.NewFromConfig(cfg)
 	httpClient := &http.Client{}
@@ -48,6 +52,9 @@ func main() {
 				log.Fatal(err)
 				return
 			}
+
+			msg := fmt.Sprintf("❗️ URL %v has changed", *track.URL)
+			commands.SendMsg(bot, track.ChatID, msg)
 
 			if result.State == domain.Updated {
 				fmt.Printf("Diff size: %v\n", result.DiffSize)
